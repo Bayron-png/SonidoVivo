b90c87b6bd9ae2cbc1c932308e1831e8b955c947
Bayron Urrutia <bay.urrutia@duocuc.cl>
2026-09-25 18:50:53 +0000

    FEAT: Atomo|Email Input.jsx

---
+++ SonidoVivo/src/components/atoms/EmailInput.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+
+const EmailInput = ({ value, onChange }) => (
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label className="fw-bold">Correo</Form.Label>
+    <Form.Control 
+      type="email" 
+      placeholder="Ingrese su correo" 
+      value={value} 
+      onChange={onChange}
+      required 
+    />
+  </Form.Group>
+);
+
+export default EmailInput;
